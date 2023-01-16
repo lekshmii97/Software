e35ffcb713e3724e28370269dae902d5d54fa4c9
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-16 20:52:41 +0530

    Welcome

---
+++ inheritance/src/pablo1/PabloClass.java
@@ -0,0 +1,15 @@
+package pablo1;
+
+public class PabloClass {
+	public void add()
+	{
+		System.out.println("Lets welcome");
+	}
+
+	public static void main(String[] args) {
+	
+PabloClass ob=new PabloClass();
+ob.add();
+	}
+
+}
