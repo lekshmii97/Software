ebaa67f7790194229bec199a6a03c89d310b802c
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-17 12:31:56 +0530

    Mthod commit

---
+++ inheritance/src/supergirl/SuperMethods.java
@@ -0,0 +1,26 @@
+package supergirl;
+
+public class SuperMethods extends SuperMethos
+ {
+int a=40;
+int b=60;
+
+public void added()
+{
+	int sum=a+b;
+	System.out.println("sum="+sum);
+	System.out.println(super.a);
+}
+	public static void main(String[] args)
+	
+	{
+		SuperMethods ob=new SuperMethods();
+		
+				ob.added();
+				ob.add();
+				
+		
+
+	}
+
+}
---
+++ inheritance/src/supergirl/SuperMethos.java
@@ -0,0 +1,15 @@
+package supergirl;
+
+public class SuperMethos
+{
+int a=10;
+int b=20;
+
+
+public void add()
+{
+
+int sum=a+b;
+System.out.println("sum="+sum);
+}
+}
