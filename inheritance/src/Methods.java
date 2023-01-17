4e8a6867cc16e1b577f6001f6a189200449d7b45
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-17 12:35:09 +0530

    commit

---
+++ inheritance/src/Methods.java
@@ -0,0 +1,19 @@
+
+public class Methods {
+public void add()
+{
+	System.out.println("lekshmi");
+}
+private void sub()
+{
+	System.out.println("bhavika");
+}
+protected void mul()
+{
+	System.out.println("vasishta");
+}
+void div()//default
+{
+	System.out.println("parvathy");
+}
+}
