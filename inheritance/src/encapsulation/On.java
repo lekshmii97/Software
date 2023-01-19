089b83e6c0f93d5c5a2bd5c6168385c007a490b0
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-19 11:49:31 +0530

    onoff

---
+++ inheritance/src/encapsulation/On.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class On {
+
+	public static void main(String[] args) {
+		Off sc=new Off();
+		sc.setName("sree");
+		sc.setAge(12);
+		
+		System.out.println(sc.getName());
+		System.out.println(sc.getAge());
+	}
+
+}
