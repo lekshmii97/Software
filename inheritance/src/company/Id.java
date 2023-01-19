12c041cf1f507af943f916cd2b336d6fa2dabbaa
Zartech <Zartech@DESKTOP-CN5728E>
2023-01-19 11:15:28 +0530

    employee commit

---
+++ inheritance/src/company/Id.java
@@ -0,0 +1,12 @@
+package company;
+
+public class Id {
+public String name;
+public int id;
+
+public Id(String name,int id)
+{
+	this.name=name;
+	this.id=id;
+}
+}
---
+++ inheritance/src/company/Id2.java
@@ -0,0 +1,29 @@
+package company;
+
+public class Id2 {
+
+	String companyname;
+	int salary;
+	Id sd;
+	
+	public Id2(String companyname,int salary,Id sd)
+	
+	{
+		this.companyname=companyname;
+		this.salary=salary;
+		this.sd=sd;
+	
+	}
+	public void print()
+	{
+		System.out.println(sd.name+" "+sd.id);
+		System.out.println(companyname+" "+salary);
+	}
+	public static void main(String[] args) {
+		Id sc=new Id("Lekshmi",3);
+Id2 sc1=new Id2("tcs",35000,sc);
+sc1.print();
+
+	}
+
+}
