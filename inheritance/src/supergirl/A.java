b56c8c073b30e74e827e719609b797facbabc69f
Zartech <Zartech@DESKTOP-CN5728E>
2023-04-07 10:27:19 +0530

    revision

--- inheritance/src/supergirl/A.java
+++ inheritance/src/supergirl/A.java
@@ -11,7 +11,7 @@ public A(int X,int Y)
 }
 public void add()
 {
-	c=a+b;
+	c=a+b;https://www.flipkart.com/ivbox-breakfast-combo-appam-maker-uttapam-pan-paniarakkal-set-lid-2-l-capacity-24-cm-diameter/p/itm51d9d91c6d3f2?pid=PTPG29YHNYZZJZFV&lid=LSTPTPG29YHNYZZJZFVAY3MUW&marketplace=FLIPKART&store=upp%2Ftnx%2F9fq&srno=b_1_1&otracker=nmenu_sub_Home%20%26%20Furniture_0_Pans&fm=search-autosuggest&iid=en_h1z9rPmGkqV5nGLZPsBXIqpoP0hx2Ry6snHcqaxVDoyCgmcMdFNgko3n5sLdo2LZGqwOdt9dNmvs1O1tKWmJvg%3D%3D&ppt=browse&ppn=browse&ssid=lkplqm70n40000001680778702594
 		System.out.println(c);
 }
 }
